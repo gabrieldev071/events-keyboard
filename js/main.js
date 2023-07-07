54c74bda6a10bfa3bce05d3ebd800ad640f0b164
Gabriel Souza <imgabriel.souza071@gmail.com>
2023-07-06 21:11:17 -0300

    docs: update js e css

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
-function keyFunction(index) {
+function keyFunction(index, text) {
   console.log(event.keyCode);
 
   //concatenando e selecionando elemento com base no index/ser digitado
+
   let valor = document.getElementById("input" + index).value;
   let res = document.getElementById("resultado");
-  res.innerHTML = valor
-/*   const element = document.createElement("p");
   res.appendChild(element);
-  element.innerHTML = valor; */
+  element.classList.add("inputP");
+  element.innerHTML = valor; 
 } 
+
+
+const element = document.createElement("p");
